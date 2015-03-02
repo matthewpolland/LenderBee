928c41cd0d31ed6e3a471d6b28f63a96ed96a5ab
matthewpolland <matthewpolland@gmail.com>
2015-03-02 15:37:30 -0800

    (feat) replace carousel with random image page

--- client/src/components/search/Carousel.react.jsx
+++ client/src/components/search/Carousel.react.jsx
@@ -3,6 +3,31 @@ var Reflux = require('reflux');
 var carouselStore = require('../../stores/CarouselStore.js');
 var actions = require('../../actions/actions.js');
 var searchStore = require('../../stores/searchStore');
+
+var InnerImage = React.createClass({
+
+  propTypes: {
+    itemInfo: React.PropTypes.object
+  },
+
+  handleClick: function() {
+   actions.selectItem(this.props.itemInfo, this.props.itemInfo.lender);
+  },
+
+  render: function(){
+    console.log("HO HO HO: ", this.props.itemInfo);
+    return (
+      <div>
+        <Link to="SingleItem" onClick={this.handleClick}>
+          <p>{this.props.itemInfo.title}</p>
+          <img src={this.props.itemInfo.imageurl} alt={this.props.itemInfo.title} />
+          <p>{this.props.itemInfo.beebucks}</p>
+        </Link>
+      </div>
+    )
+  }
+})
+
 var carousel = React.createClass({
 
   mixins: [Reflux.connect(carouselStore)],
@@ -16,20 +41,24 @@ var carousel = React.createClass({
   },
 
   render: function() {
-      
-      return (
-      <div className="carousel">
-      <p>{this.state}</p>
-      <img src="#" href="#" alt="previous item set" className="inline arrow" onClick={this.handleLeftArrowClick}/>
-      <ul className="inline">
-        <li><img src="#" href="#" alt="image A"/></li>
-        <li><img src="#" href="#" alt="image B"/></li>
-        <li><img src="#" href="#" alt="image C"/></li>
-      </ul>
-      <img src="#" href="#" alt="next item set" className="inline arrow" onClick={this.handleRightArrowClick}/>
+    var items = this.state.items.map(function(item) {
+      return (<InnerImage itemInfo={item} />);
+    });
+    return (
+    <div className="carousel">
+      {items}
     </div>
     )
   }
 })
 
 module.exports = carousel;
+
+// <p>{this.state}</p>
+// <img src="#" href="#" alt="previous item set" className="inline arrow" onClick={this.handleLeftArrowClick}/>
+// <ul className="inline">
+//   <li><img src="#" href="#" alt="image A"/></li>
+//   <li><img src="#" href="#" alt="image B"/></li>
+//   <li><img src="#" href="#" alt="image C"/></li>
+// </ul>
+// <img src="#" href="#" alt="next item set" className="inline arrow" onClick={this.handleRightArrowClick}/>
